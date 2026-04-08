f50bf4a304f6f5dd07b6714478619ad6acbaccf0
robersygonzalez <robersygonzalez@gmail.com>
2026-04-08 15:20:10 -0400

    Fin seccion 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,14 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  // isInt, IsPositive, min 1
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  // isString, Minlength 1
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
