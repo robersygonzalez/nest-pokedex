352469419dcb11a759b7df3c433775d6b7329f9b
robersygonzalez <robersygonzalez@gmail.com>
2026-04-13 12:13:56 -0400

    video 109 end

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
-  port: process.env.PORT || 3002,
+  port: process.env.PORT || 10000,
   defaultlimit: process.env.DEFAULT_LIMIT || 7,
 });
